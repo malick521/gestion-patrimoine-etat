31a501b1335f1cdb3cd95f26e896552cc8c7891f
Malick <24009@supnum.mr>
2026-05-16 23:27:28 +0000

    Initial commit Backend

---
+++ src/test/java/mr/patrimoine/gestion/GestionApplicationTests.java
@@ -0,0 +1,13 @@
+package mr.patrimoine.gestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
